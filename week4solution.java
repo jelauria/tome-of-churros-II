53889889804e3cd1978e97eb22f6ee052531595c
Joyce Elauria <jaelauria@gmail.com>
2020-07-16 16:09:01 -0700

    week 4 in-class solution

---
+++ week4solution.java
@@ -0,0 +1,46 @@
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+/*
+Given a list of Strings and a target character, return the largest sub-list of Strings
+sharing the same frequency of the target character, ignoring case.
+
+Example:
+Input: {apple, orange, bannana, a, Android, purple, America, Amazon}
+       target = ‘a’
+Output: {apple, orange, a, Android}
+*/
+
+public class Solution {
+    public List<String> groupFinder(char target, List<String> data) {
+        Map<Integer, List<String>> groups = new HashMap<>();
+        for(String s : data) {
+            Integer key = this.getKey(target, s);
+            if (!groups.containsKey(key)) {
+                groups.put(key, new LinkedList<>());
+            }
+            groups.get(key).add(s);
+        }
+        int max = 0;
+        List<String> result = null;
+        for (Integer key : groups.keySet()) {
+            List<String> group = groups.get(key);
+            if (group.size() > max) {
+                max = group.size();
+                result = group;
+            }
+        }
+        return result;
+    }
+    private int getKey(char target, String data) {
+        int counter = 0;
+        for (int i = 0; i < data.length(); i++) {
+            if (Character.toLowerCase(data.charAt(i)) == Character.toLowerCase(target)) {
+                counter++;
+            }
+        }
+        return counter;
+    }
+}
